eacbc3f52802ba8c7903d354421fc1fb786894af
Taha Naveed <taha.naveed@kellogg.northwestern.edu>
2025-02-25 19:17:15 +0000

    updated

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path, notice: "Place was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name)  # Ensure form sends 'place[name]'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  resources :places, only: [:index, :new, :create, :show] do
+    resources :entries, only: [:new, :create]
+  end
 end
