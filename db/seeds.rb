fc782f7bbddbfb8ce39560d1747ecabbf388e86c
Taha Naveed <taha.naveed@kellogg.northwestern.edu>
2025-02-25 20:12:18 +0000

    final updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Place.create(name: "London")
+Place.create(name: "Islamabad")
+Place.create(name: "Evanston")
+Place.create(name: "Karachi")
